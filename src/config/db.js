d66b0c151bd11aa08e3578217f8f9c69353a11a7
EmersonLaranja <emersonlaranja@gmail.com>
2020-07-22 09:47:26 -0300

    .env updates

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,11 @@
 const { Pool } = require('pg');
+require('dotenv').config();
 
 module.exports = new Pool({
-  user: 'postgres',
-  password: 'laranja',
-  host: 'localhost',
-  port: 5432,
-  database: 'gymmanager',
+  // user: 'postgres',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DATABASE,
 });
